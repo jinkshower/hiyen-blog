08fc378e7a17bef1b347a0a0679ca22ec345a735
jinkshower <askthem@naver.com>
2023-12-24 22:51:33 +0900

    adjust profile pic

--- blog-config.js
+++ blog-config.js
@@ -4,7 +4,7 @@ module.exports = {
   author: "Hiyen",
   siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
   links: {
-    github: "https://github.com/devHudi",
+    github: "https://github.com/jinkshower",
   },
   utterances: {
     repo: "devHudi/gatsby-starter-hoodie",
