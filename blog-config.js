245d5856241069ff809179814143e291b4777ade
jinkshower <askthem@naver.com>
2023-12-24 23:00:10 +0900

    test

--- blog-config.js
+++ blog-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   title: "Hiyen",
-  description: "Hello :) I'm Hiyen, a backend developer",
+  description: "Hello :) I'm Hiyen, a backend developer!",
   author: "Hiyen",
   siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
   links: {
-    github: "https://github.com/jinkshower",
+    github: "https://github.com/jinkshower"
   },
   utterances: {
     repo: "devHudi/gatsby-starter-hoodie",
